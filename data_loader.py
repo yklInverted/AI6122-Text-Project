fdb7b49c1eb2efb51a6add530647b418c357da09
yklInverted <greencake.lky@gmail.com>
2022-09-23 20:35:54 +0800

    Dataset Sampling & Tokenization

---
+++ data_loader.py
@@ -0,0 +1,27 @@
+import nltk
+import pandas as pd
+import sys
+import os
+
+class DataLoader():
+
+    data_path1 = "./data/Kindle_Store_5.json"
+    data_path2 = "./data/Digital_Music_5.json"
+
+    def __init__(self, data_path = data_path1) -> None:
+        
+        self.table = pd.read_json(data_path, 'records', lines = True);
+
+
+    def load_table(self, samples = 200):
+
+        samples = self.table.sample(samples)
+        return samples.reset_index(drop = True, inplace = False)
+
+    def load_review_text(self, samples = 200):
+
+        reviews = self.table.sample(samples)['reviewText']
+        return reviews.to_numpy()
+
+    
+
